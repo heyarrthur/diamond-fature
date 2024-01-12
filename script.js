7e53fb0ae5d305c635234d1149bdd1f156b684ee
arthurmf35 <147673248+heyarrthur@users.noreply.github.com>
2024-01-12 16:28:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-
-    // Adicione aqui a lógica de autenticação. Por exemplo, você pode comparar o username e password com dados no servidor.
-
-    // Exemplo básico de verificação
-    if (username === 'usuario' && password === 'senha') {
+    
+    // Verifica o primeiro conjunto de credenciais
+    if (username === 'dm.marquesferreira@gmail.com' && password === '0201davi') {
         alert('Login bem-sucedido!');
         window.location.href = 'faturament/faturament.html'; // Redireciona para a página após o login bem-sucedido
-    } else {
+    } 
+    // Verifica o segundo conjunto de credenciais
+    else if (username === 'admin' && password === 'admin') {
+        alert('Login bem-sucedido!');
+        window.location.href = 'faturament/faturament.html'; // Redireciona para a página após o login bem-sucedido
+    } 
+    // Caso nenhum conjunto corresponda, exibe uma mensagem de erro
+    else {
         alert('Login falhou. Verifique seu nome de usuário e senha.');
     }
 }
